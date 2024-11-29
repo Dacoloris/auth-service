46fa55ffbaeb2f0f8b8445257ea8ab46ab35af74
Данил <dacoloris@gmail.com>
2024-11-29 15:05:02 +0700

    refactor

---
+++ mock/mockEmailNotifier.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+type MockEmailNotifier struct{}
+
+func (m *MockEmailNotifier) SendWarningEmail(email string, oldIP string, newIP string) error {
+	fmt.Printf("Warning: User with email %s has changed IP from %s to %s\n", email, oldIP, newIP)
+	return nil
+}
